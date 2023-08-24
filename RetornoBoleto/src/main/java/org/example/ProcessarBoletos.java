b9e3319fef049e63098d7f4f239ba2e52e5a38e4
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-23 21:39:36 -0300

    Adicionado programaçao funcional

--- RetornoBoleto/src/main/java/org/example/ProcessarBoletos.java
+++ RetornoBoleto/src/main/java/org/example/ProcessarBoletos.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import java.net.URI;
+import java.util.List;
+import java.util.function.Function;
 
 public class ProcessarBoletos {
-    private LeituraRetorno leituraRetorno;
 
-    public ProcessarBoletos(LeituraRetorno leituraRetorno) {
+    private Function<URI, List<Boleto>> leituraRetorno;
+
+    public ProcessarBoletos(Function<URI, List<Boleto>> leituraRetorno) {
         this.leituraRetorno = leituraRetorno;
     }
 
@@ -13,7 +16,7 @@ public class ProcessarBoletos {
         leituraRetorno.lerArquivo(nomeArquivo);
     }
 
-    public void setLeituraRetorno(LeituraRetorno leituraRetorno) {
+    public void setLeituraRetorno(Function<URI, List<Boleto>> leituraRetorno) {
         this.leituraRetorno = leituraRetorno;
     }
 
