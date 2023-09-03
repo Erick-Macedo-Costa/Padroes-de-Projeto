7f9ee48919b6ea4033196c1eff9510cf2ff56f7f
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-09-03 16:00:14 -0300

    Corregindo erro Main

--- RetornoBoleto/src/main/java/Main.java
+++ RetornoBoleto/src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.net.URISyntaxException;
 public class Main {
     public static void main(String[] args) throws URISyntaxException {
         var processarBoletos = new LeituraRetornoBancoBrasil();
-        var processarBoletosBradesco = new LeituraRetornoBradesco());
+        var processarBoletosBradesco = new LeituraRetornoBradesco();
         var uriBB = Main.class.getResource("banco-brasil-1.csv").toURI();
         var uriB = Main.class.getResource("bradesco-1.csv").toURI();
         System.out.println("------------------- BANCO DO BRASIL -------------------");
